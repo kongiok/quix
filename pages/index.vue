9f6f325fed1fe5896963588e5e48278c2204eefb
Marx Wang <211410146@o365.tku.edu.tw>
2025-01-08 23:19:37 +0800

    chore(project): add nuxt content

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,6 @@
   <main
     class="h-screen w-screen flex flex-col items-center justify-center bg-neutral-100"
   >
-    <h1 class="text-2xl text-primary-600">moneybara</h1>
+    <h1 class="text-2xl text-primary-600 capitalize">quix</h1>
   </main>
 </template>
